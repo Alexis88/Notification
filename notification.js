5e12afe3a2f400bd388da56e37f14819d232108f
Alexis López <Alexis88@users.noreply.github.com>
2023-06-04 10:05:58 -0500

    Update notification.js
    New version!

--- notification.js
+++ notification.js
@@ -1,126 +1,106 @@
 /**
- * NOTIFICACIONES PERSONALIZADAS
+ * NOTIFICACIONES MINIMALISTAS
  * 
- * Este script genera un cuadro de notificación. Se muestra en la esquina inferior 
- * izquierda de la pantalla del dispositivo, ocultándose automáticamente después de 
- * cuatro segundos o pulsando sobre la notificación. Puede mostrar varias notificaciones 
- * a la vez, las cuales se irán mostrando una sobre otra e irán desplazándose hacia abajo 
- * conforme las notificaciones anteriores vayan ocultándose.
+ * Plugin que genera notificaciones personalizadas de estilo minimalista
  * 
- * MODO DE USO: Notification.msg("Texto a mostrar"/{Objeto de opciones de configuración});
+ * MODO DE USO:
  * 
- * 
+ * Notification.msg("Texto de ejemplo");
+ * Notification.msg({
+ * 		text: "Texto de ejemplo",
+ * 		background: true,
+ * 		time: 3000,
+ * 		keep: false,
+ * 		onShow: _ => {
+ * 			//Esto se ejecutará luego de haberse mostrado la notificación
+ * 		},
+ * 		onHide: _ => {
+ * 			//Esto se ejecutará luego de haberse ocultado la notificación
+ * 		}
+ * });
+ *
+ * @param		{options}		Object/String
  * @author		Alexis López Espinoza
- * @version		3.0
- * @param		options			Plain Object/String
+ * @version		1.0
+ * @date 		2023-06-04
  */
 
 "use strict";
 
 const Notification = {
-	msg: options => {
-		/*** OPCIONES DE CONFIGURACIÓN ***
-		 * 
-		 * options.texto: El texto a mostrar
-		 * options.callback: La llamada de retorno a ejecutarse luego de ocultarse la notificación
-		 * options.background: Determina si se mostrará un fondo oscuro mientra se muestra la notificación
-		 * options.time: El tiempo que se mostrará la notificación
-		 * options.keep: Determina si la notificación se mostrará permanentemente
-		 */
-
-		//Se libera la memoria ocupada por la configuración anterior (si la hubo)
-		delete Notification.options;
-
-		//Si se recibió algún argumento
-		if (options){
-			//Si el argumento no es un objeto, se lo establece como el texto a mostrar
-			if ({}.toString.call(options) !== "[object Object]"){
-				//Texto a mostrar
-				Notification.text = options;
-			}
-			//Si el argumento es un objeto, se lo establece como configuración de la notificación
-			else{
-				//Opciones de configuración
-				Notification.options = options;
-
-				//Texto a mostrar
-				Notification.text = options.texto;
-			}
+	/**
+	 * options.text: Texto a mostrar
+	 * options.background: Establece un fondo oscuro detrás de la notificación
+	 * time: Tiempo en el que permanecerá visible la notificación
+	 * keep: Establece que la notificación se muestre permanentemente
+	 * onShow: Llamada de retorno a ejecutarse luego de mostrarse la notificación
+	 * onHide: Llamada de retorno a ejecutarse luego de ocultarse la notificación
+	 */
+	msg(options){
+		if (!options || !["[object String]", "[object Object]"].includes(Notification.type(options))){
+			throw new Error("Tiene que establecer un contenido para la notificación");
 		}
-		//Caso contrario, se aborta la ejecución
+
+		if (Notification.type(options) == "[object String]"){
+			Notification.text = options;
+			Notification.background = false;
+			Notification.time = 3000;
+			Notification.keep = false;
+			Notification.onShow = null;
+			Notification.onHide = null;
+		}
 		else{
-			throw new Error("Tiene que añadir un texto o un objeto con opciones de configuración para poder mostrar la notificación");
-		}		
+			Notification.text = options.text;
+			Notification.background = options.background || false;
+			Notification.time = options.time || 3000;
+			Notification.keep = options.keep || false;
+			Notification.onShow = options.onShow && Notification.isFunction(options.onShow) ? options.onShow : null;
+			Notification.onHide = options.onHide && Notification.isFunction(options.onHide) ? options.onHide : null;
+		}
 
-		//Llamada de retorno
-		Notification.callback = options.callback && {}.toString.call(options.callback) === "[object Function]" ? options.callback : null;
-
-		//Fondo
-		Notification.background = options.background || false;
-
-		//Tiempo por defecto durante el cual se mostrará la notificación
-		Notification.time = options.time || 3000;
-
-		//Determinar si la notificación se mostrará permanentemente
-		Notification.keep = options.keep || false;
-
-		//Se ejecuta el método que muestra el cuadro de notificación
-		Notification.show();
+		Notification.queue = Notification.queue || [];
+		const cloneConfig = Notification.createNotification();		
+		Notification.events(cloneConfig);
 	},
 
-	show: _ => {
-		//Cuadro de la notificación
-		Notification.createBox();
-
-		//Si se ha establecido un fondo, se genera uno
-		Notification.background && Notification.createBack();
-
-		//Se genera una copia de la configuración de la notificación
-		const copy = {...Notification};
-
-		//Se recupera o genera una nueva cola de notificaciones
-		Notification.queue = Notification.queue || [];		
-
-		//Se encola la notificación
-		Notification.queue.push(copy);
-
-		//Si se ha establecido un fondo, se lo añade
-		Notification.background && Notification.addBackground(copy);
-
-		//Se muestra el cuadro de notificación
-		Notification.addBox(copy);
-
-		//Se verifica si la notificación se mostrará por unos segundos o permanentemente
-		Notification.showTime(copy);
-
-		//Configuración de eventos
-		Notification.events(copy);		
+	type(elem){
+		return {}.toString.call(elem);
 	},
 
-	events: boxConfig => {
-		//Se oculta la notificación en caso de que se haga clic sobre ella
-		boxConfig.box.addEventListener("click", _ => Notification.hide(boxConfig), false);
-
-		//Se ocultan la notificación y el fondo en caso de que se haga clic sobre este último
-		boxConfig.back?.addEventListener("click", _ => Notification.hide(boxConfig), false);
-
-		//Se mostrará el cursor con forma de mano cuando se pose el cursor sobre la notificación
-		boxConfig.box.addEventListener("mouseover", _ => boxConfig.box.style.cursor = "pointer", false);
-
-		//Se retirará el cursor con forma de mano cuando se retire el cursor de la notificación
-		boxConfig.box.addEventListener("mouseout", _ => boxConfig.box.style.cursor = "auto", false);
-
-		//Al girar el dispositivo, cambiarán las dimensiones del fondo
-		window.addEventListener("orientationchange", _ => Notification.resizeBack(boxConfig), false);
-		window.addEventListener("resize", _ => Notification.resizeBack(boxConfig), false);
+	isFunction(fn){
+		return Notification.type(fn) === "[object Function]";
 	},
 
-	createBox: _ => {
-		Notification.box = document.createElement("span");
-		Notification.box.classList.add("notification-box");
-		Notification.box.style = `
+	events(config){
+		config.box.addEventListener("click", _ => Notification.hide(config), false);
+		window.addEventListener("resize", Notification.resize, false);
+		window.addEventListener("orientationchange", Notification.resize, false);
+	},
+
+	createNotification(){
+		const 
+			back = document.createElement("div"),
+			box = document.createElement("span"),
+			width = window.innerWidth,
+			height = window.innerHeight;
+
+		back.style = `
+			position: absolute;
+			background-color: rgba(0, 0, 0, .6);
+			display: flex;
+			align-items: center;
+			width: ${width}px;
+			height: ${height}px;
+			top: 0;
+			left: 0;
+			justify-content: center;
+			z-index: 8888;
+			transition: all ease .4s;
+		`;
+
+		box.style = `
 			background-color: #FFFFEF;
-			width: ${window.innerWidth >= 850 ? "250px" : "200px"};
+			width: ${width >= 850 ? "250px" : "200px"};
 			padding: 1rem .75rem;
 			display: flex;
 			align-items: center;
@@ -134,115 +114,105 @@ const Notification = {
 			overflow-x: hidden;
 			overflow-y: auto;
 			box-shadow: 10px 10px 20px 5px gray;
+			cursor: pointer;
 			transition: .4s ease;
 			z-index: 9999;
 		`;
+		box.innerHTML = Notification.text;
+
+		Notification.back = back;
+		Notification.box = box;	
+		const cloneConfig = {...Notification};
+		Notification.queue.push(cloneConfig);
+
+		if (cloneConfig.background){
+			document.body.append(cloneConfig.back);
+		}
+		document.body.append(cloneConfig.box);
+
+		Notification.bottom(cloneConfig);
+
+		setTimeout(_ => {		
+			cloneConfig.back.style.opacity = .6;
+			cloneConfig.box.style.left = 0;			
+			cloneConfig.onShow && cloneConfig.onShow();
+		}, 400);
+
+		if (!cloneConfig.keep){
+			cloneConfig.timer = setTimeout(_ => cloneConfig.hide(cloneConfig), cloneConfig.time);
+		}
+
+		return cloneConfig;
 	},
 
-	addBox: boxConfig => {
-		//Se añade el texto al cuadro de notificación
-		boxConfig.box.innerHTML = boxConfig.text;
+	resize(){
+		Notification.queue.forEach(config => {
+			if (config.background){
+				const
+					back = config.back,
+					scroll = document.documentElement.scrollTop || document.body.scrollTop,
+					width = window.innerWidth,
+					height = window.innerHeight;
 
-		//Se añade el cuadro de notificación al documento
-		document.body.appendChild(boxConfig.box);
+				back.style.top = `${scroll}px`;
+				back.style.width = `${width}px`;
+				back.style.height = `${height}px`;
+			}
+		});
+	},
+
+	hide(config){
+		const 
+			back = config.back,
+			box = config.box,
+			index = Notification.queue.indexOf(config);
+
+		let isBackConfig = 0;
+
+		config.background && back.animate([
+			{opacity: .6},
+			{opacity: 0}
+		], {duration: 400, fill: "forwards"});
+
+		box.animate([
+			{left: 0},
+			{left: "-30rem"}
+		], {duration: 400, fill: "forwards"});
+
+		config.onHide && config.onHide();
+		config.timer && clearTimeout(config.timer);
 
 		setTimeout(_ => {
-			//Se muestra la notificación
-			boxConfig.box.style.left = 0;
-			
-			//Se posiciona la notificación verticalmente
-			Notification.bottom(boxConfig);
+			config.background && back.remove();
+			box.remove();
+			config.onHide && config.onHide();
+			Notification.queue.splice(index, 1);
+
+			if (!Notification.queue.length){
+				document.body.style.overflow = "auto";
+			}
+			else{
+				Notification.queue.forEach(boxConfig => {
+					isBackConfig += boxConfig.background ? 1 : 0;
+					Notification.bottom(boxConfig);
+				});
+
+				if (!isBackConfig){
+					document.body.style.overflow = "auto";
+				}
+			}
 		}, 400);
 	},
 
-	createBack: _ => {
-		Notification.back = document.createElement("div");
-		Notification.back.style = `
-			width: ${window.innerWidth * 50}px;
-			height: ${window.innerHeight * 50}px;
-			margin: 0;
-			background-color: #000;
-			opacity: 0;
-			transition: .4s ease;
-			position: absolute;
-			top: 0;
-			left: 0;
-			z-index: 8888;
-		`;
-	},
+	bottom(config){
+		const order = Notification.queue.indexOf(config);
 
-	addBackground: boxConfig => {
-		//Se realiza una copia del valor de la propiedad "overflow" del documento
-		boxConfig.overflow = getComputedStyle(document.body).overflow;
-
-		//Se añde el fondo al documento
-		document.body.appendChild(boxConfig.back);
-
-		//Se ocultan las barras de desplazamiento del documento
-		document.body.style.overflow = "hidden";
-
-		//Se le da visibilidad al fondo
-		setTimeout(_ => boxConfig.back.style.opacity = .6, 400);
-	},
-
-	showTime: boxConfig => {
-		if (!boxConfig.keep){
-			boxConfig.timer = setTimeout(_ => Notification.hide(boxConfig), boxConfig.time);
-		}
-	},
-
-	exists: _ => document.querySelectorAll(".notification-box"),
-
-	hide: boxConfig => {
-		boxConfig.box.style.left = "-30rem";
-
-		if (boxConfig.background){
-			boxConfig.back.style.opacity = 0;
-		}
-
-		if (boxConfig.timer){
-			clearTimeout(boxConfig.timer);
-		}
-
-		if (boxConfig.callback){
-			boxConfig.callback();
-		}
-
-		setTimeout(_ => {
-			boxConfig.box.remove();			
-
-			if (boxConfig.background){
-				boxConfig.back.remove();
-				document.body.style.overflow = boxConfig.overflow || "auto";
-			}
-
-			if (Notification.queue.length){
-				Notification.queue.forEach(boxConfig => Notification.bottom(boxConfig));
-			}
-
-			Notification.queue.splice(Notification.queue.indexOf(boxConfig), 1);
-		}, 400);
-	},
-
-	hideAll: _ => Notification.queue.forEach(boxConfig => Notification.hide(boxConfig)),
-
-	resizeBack: boxConfig => {
-		if (boxConfig.background){
-			boxConfig.back.style.width = `${window.innerWidth * 50}px`;
-			boxConfig.back.style.height = `${window.innerHeight * 50}px`;
-			boxConfig.back.style.top = 0;
-		}
-	},
-
-	bottom: boxConfig => {
-		const order = Notification.queue.indexOf(boxConfig);
-
-		boxConfig.box.style.bottom = ((order, queue) => {
+		config.box.style.bottom = ((order, queue) => {
 			let totalHeight = 0;
 
 			if (order){
 				for (let i = 0; i < order; i++){
-					totalHeight += queue[i].box.offsetHeight + 8;
+					totalHeight += queue[i].box.offsetHeight + 4;
 				}
 			}
 			else if (order > -1){
